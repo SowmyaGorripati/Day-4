287dd003508b237c031765966599b0e654c7a497
SowmyaGorripati <116424393+SowmyaGorripati@users.noreply.github.com>
2022-11-06 12:19:22 +0530

    Update and rename Compare 2 JSON to Compare 2 JSON.js

--- Compare 2 JSON.js
+++ Compare 2 JSON.js
@@ -1,7 +1,9 @@
-1. How to compare two JSON have the same properties without order?
+/*1. How to compare two JSON have the same properties without order?
    a. var obj1 = { name: "Person 1", age:5 };
    b. var obj2 = { age:5, name: "Person 1" };
-CODE: 
+*/   
+
+//CODE: 
 var obj1 = {name:"Person 1", age : 5};
 var obj2 = {age : 5, name : "Person 1"};
 console.log(JSON.stringify(obj1)===JSON.stringify(obj2))
